2978319f27108506a421d3c647c512ad66b0442c
Lennert Claeys <claeyslennert@gmail.com>
2023-11-26 09:38:55 +0100

    Add atlasPacker

---
+++ scripts/atlasPacker.ts
@@ -0,0 +1,173 @@
+import Jimp from "jimp";
+import { Block, GrowingPacker, Resolution } from "binpacking";
+import fs from "fs";
+import { Atlas, Frame } from "./util";
+
+interface SpriteBlock extends Block {
+  jimp: Jimp;
+  name: string;
+  fit?: Resolution;
+  animation?: string;
+}
+
+const blocks: SpriteBlock[] = [];
+
+const DIRECTORY = "./public/atlas/";
+
+// This might help with edges containing colors of adjacent sprites in the atlas?
+// @todo this doesn't really work for margins > 1
+const MARGIN = 1;
+
+const contents = fs.readdirSync(DIRECTORY);
+const files = contents.filter((file) => file.endsWith(".json"));
+const folders = contents.filter((file) => !file.includes("."));
+
+for (let folder of folders) {
+  const files = fs.readdirSync(DIRECTORY + folder);
+  console.log(files);
+
+  for (let file of files) {
+    const jimp = await Jimp.read(`${DIRECTORY}${folder}/${file}`);
+
+    const parts = file.split(".")[0].split("_");
+
+    blocks.push({
+      name: `${folder}_${file}`,
+      w: jimp.getWidth() + MARGIN * 2,
+      h: jimp.getHeight() + MARGIN * 2,
+      jimp,
+      ...(!isNaN(Number(parts[1])) && { animation: `${folder}_${parts[0]}` }),
+    });
+  }
+}
+
+for (let file of files) {
+  let atlas: Atlas;
+
+  try {
+    atlas = JSON.parse(fs.readFileSync(DIRECTORY + file).toString());
+  } catch (error) {
+    console.error(`Failed to load atlas for ${file}`, error);
+    process.exit(1);
+  }
+
+  let image: Jimp;
+
+  try {
+    image = await Jimp.read(DIRECTORY + atlas.meta.image);
+  } catch (error) {
+    console.error(`Failed to load image for atlas ${file}`, error);
+    process.exit(1);
+  }
+
+  Object.entries(atlas.frames).forEach(([name, { frame }]) => {
+    const jimp = new Jimp(frame.w, frame.h).blit(
+      image,
+      0,
+      0,
+      frame.x,
+      frame.y,
+      frame.w,
+      frame.h
+    );
+
+    blocks.push({
+      name,
+      w: frame.w + MARGIN * 2,
+      h: frame.h + MARGIN * 2,
+      jimp,
+    });
+  });
+}
+
+blocks.sort((a, b) => b.h - a.h);
+
+const packer = new GrowingPacker();
+packer.fit(blocks);
+
+const frames: Record<string, Frame> = {};
+const animations: Record<string, string[]> = {};
+const atlas = new Jimp(packer.root.w, packer.root.h);
+for (let block of blocks) {
+  if (block.fit!.used) {
+    atlas
+      .blit(block.jimp, block.fit!.x + MARGIN, block.fit!.y + MARGIN)
+      .blit(
+        block.jimp,
+        block.fit!.x + MARGIN,
+        block.fit!.y,
+        0,
+        0,
+        block.w - MARGIN * 2,
+        MARGIN
+      )
+      .blit(
+        block.jimp,
+        block.fit!.x + MARGIN,
+        block.fit!.y + block.h - MARGIN,
+        0,
+        block.h - MARGIN * 3,
+        block.w - MARGIN * 2,
+        MARGIN
+      )
+      .blit(
+        block.jimp,
+        block.fit!.x,
+        block.fit!.y + MARGIN,
+        0,
+        0,
+        MARGIN,
+        block.h - MARGIN * 2
+      )
+      .blit(
+        block.jimp,
+        block.fit!.x + block.w - MARGIN,
+        block.fit!.y + MARGIN,
+        block.w - MARGIN * 3,
+        0,
+        MARGIN,
+        block.h - MARGIN * 2
+      );
+
+    frames[block.name] = {
+      frame: {
+        x: block.fit!.x + MARGIN,
+        y: block.fit!.y + MARGIN,
+        w: block.w - MARGIN * 2,
+        h: block.h - MARGIN * 2,
+      },
+    };
+
+    if (block.animation) {
+      animations[block.animation] = animations[block.animation] || [];
+      animations[block.animation].push(block.name);
+    }
+  } else {
+    console.error(`Block ${block.name} did not fit!`);
+    process.exit(1);
+  }
+}
+
+for (let animation in animations) {
+  const frames = animations[animation];
+
+  frames.sort(
+    (a, b) =>
+      Number(a.split(".")[0].split("_").at(-1)) -
+      Number(b.split(".")[0].split("_").at(-1))
+  );
+}
+
+const json: Atlas = {
+  frames,
+  animations,
+  meta: {
+    app: "https://github.com/lorgan3/open-td",
+    version: "1.0",
+    image: `atlas.png`,
+    size: { w: packer.root.w, h: packer.root.h },
+  },
+};
+
+await atlas.write(`./public/atlas.png`);
+fs.writeFileSync(`./public/atlas.json`, JSON.stringify(json, undefined, 2));
---
+++ scripts/util.ts
@@ -0,0 +1,74 @@
+import Jimp from "jimp";
+import fs from "fs";
+
+export interface NamedImage {
+  image: Jimp;
+  name: string;
+}
+
+export interface Frame {
+  frame: {
+    x: number;
+    y: number;
+    w: number;
+    h: number;
+  };
+}
+
+export interface Atlas {
+  frames: Record<string, Frame>;
+  animations?: Record<string, string[]>;
+  meta: {
+    app: string;
+    version: string;
+    image: string;
+    size: { w: number; h: number };
+  };
+}
+
+/**
+ * Dumb workaround because Jimp messes up the center on power of 2 images :/
+ */
+export const rotate = (jimp: Jimp, deg: number) => {
+  return jimp
+    .scale(3, Jimp.RESIZE_NEAREST_NEIGHBOR)
+    .rotate(deg, false)
+    .scale(1 / 3, Jimp.RESIZE_NEAREST_NEIGHBOR);
+};
+
+export const write = async (images: NamedImage[], name: string) => {
+  const actualSize = images[0].image.getWidth();
+  const width = Math.ceil(Math.sqrt(images.length));
+  const out = new Jimp(actualSize * width, actualSize * width);
+
+  const frames: Record<string, Frame> = {};
+  images.forEach(({ image, name }, i) => {
+    const x = (i % width) * actualSize;
+    const y = Math.floor(i / width) * actualSize;
+    out.blit(image, x, y);
+
+    frames[name] = {
+      frame: { x, y, w: actualSize, h: actualSize },
+    };
+  });
+
+  const json: Atlas = {
+    frames,
+    meta: {
+      app: "https://github.com/lorgan3/open-td",
+      version: "1.0",
+      image: `${name}.png`,
+      size: { w: out.getWidth(), h: out.getHeight() },
+    },
+  };
+
+  await out.write(`./public/atlas/${name}.png`);
+
+  await new Promise((resolve) =>
+    fs.writeFile(
+      `./public/atlas/${name}.json`,
+      JSON.stringify(json, undefined, 2),
+      resolve
+    )
+  );
+};
