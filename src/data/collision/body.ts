e9a53e8414a73bf454e3af81ed341c9f8dff6f9d
Lennert Claeys <claeyslennert@gmail.com>
2023-11-26 09:47:50 +0100

    Add collision detection and rudimentary physics

---
+++ src/data/collision/body.ts
@@ -0,0 +1,139 @@
+import { Level } from "../level";
+import { CollisionMask } from "./collisionMask";
+
+const GRAVITY = 0.3;
+const AIR_CONTROL = 0.6;
+const GROUND_FRICTION = 0.88;
+const AIR_FRICTION = 0.9;
+const MIN_MOVEMENT = 0.01;
+
+const SPEED = 0.15;
+const JUMP_STRENGTH = 4;
+
+export class Body {
+  private active = 1;
+
+  private xVelocity = 0;
+  private yVelocity = 0;
+
+  public x = 0;
+  public y = 0;
+
+  private _grounded = false;
+  private jumped = false;
+
+  private mask: CollisionMask;
+
+  constructor(private level: Level, width: number, height: number) {
+    this.mask = CollisionMask.forRect(width, height);
+  }
+
+  serialize() {
+    return [this.active, this.x, this.y, this.xVelocity, this.yVelocity];
+  }
+
+  deserialize(data: any[]) {
+    this.active = data[0];
+    this.x = data[1];
+    this.y = data[2];
+    this.xVelocity = data[3];
+    this.yVelocity = data[4];
+  }
+
+  walk(direction: 1 | -1) {
+    this.xVelocity += SPEED * direction * (this._grounded ? 1 : AIR_CONTROL);
+    this.active = 1;
+  }
+
+  jump() {
+    if (this.jumped) {
+      return;
+    }
+
+    this.yVelocity -= JUMP_STRENGTH;
+    this.active = 1;
+    this.jumped = true;
+  }
+
+  addVelocity(x: number, y: number) {
+    this.xVelocity += x;
+    this.yVelocity += y;
+    this.active = 1;
+  }
+
+  get grounded() {
+    return this._grounded;
+  }
+
+  tick(dt: number) {
+    if (!this.active) {
+      return;
+    }
+
+    this._grounded = false;
+    this.jumped = false;
+    this.yVelocity += GRAVITY * dt;
+
+    if (this.yVelocity > 0) {
+      this._grounded = this.level.collidesWith(
+        this.mask,
+        this.x | 0,
+        Math.ceil(this.y + this.yVelocity)
+      );
+
+      if (this._grounded) {
+        this.yVelocity = 0;
+        this.y = Math.ceil(this.y); // This isn't accurate but good enough™️
+
+        this.xVelocity *= GROUND_FRICTION;
+      } else {
+        this.xVelocity *= AIR_FRICTION;
+      }
+    } else {
+      const ceiled = this.level.collidesWith(
+        this.mask,
+        this.x | 0,
+        (this.y | 0) - 1
+      );
+
+      if (ceiled) {
+        this.yVelocity = 0;
+        this.y = Math.ceil(this.y); // This isn't accurate but good enough™️
+      }
+
+      this.xVelocity *= AIR_FRICTION;
+    }
+
+    this.x += this.xVelocity * dt;
+    this.y += this.yVelocity * dt;
+
+    if (
+      this.xVelocity !== 0 &&
+      this.level.collidesWith(this.mask, Math.floor(this.x), Math.floor(this.y))
+    ) {
+      if (
+        !this.level.collidesWith(
+          this.mask,
+          Math.floor(this.x),
+          Math.floor(this.y) - 1
+        )
+      ) {
+        this.y = Math.floor(this.y - 1); // Move up 1px slopes automatically.
+        this._grounded = true;
+      } else {
+        this.x = Math.round(this.x) - this.xVelocity;
+        this.xVelocity = 0;
+      }
+    }
+
+    if (
+      this._grounded &&
+      Math.abs(this.xVelocity) < MIN_MOVEMENT &&
+      Math.abs(this.yVelocity) < MIN_MOVEMENT
+    ) {
+      this.xVelocity = 0;
+      this.yVelocity = 0;
+      this.active -= 0.2;
+    }
+  }
+}
---
+++ src/data/collision/collisionMask.ts
@@ -0,0 +1,138 @@
+// Based on https://gist.github.com/tfry-git/3f5faa0b1c252dd1e6849da18c16570f
+
+/** Copyright 2019 by Thomas Friedrichsmeier.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
+export class CollisionMask {
+  private constructor(
+    private w: number,
+    private h: number,
+    private mask: Uint32Array[]
+  ) {}
+
+  static fromAlpha(data: ImageData) {
+    const w = data.width;
+    const h = data.height;
+    const mask: Uint32Array[] = [];
+
+    for (var y = 0; y < h; ++y) {
+      mask[y] = new Uint32Array(Math.ceil(w / 32));
+      for (var x = 0; x < w; x += 32) {
+        var bits = 0;
+        for (var bit = 0; bit < 32; ++bit) {
+          bits = bits << 1;
+          if (x + bit < w) {
+            if (data.data[(y * data.width + x + bit) * 4 + 3] > 5) {
+              bits += 1;
+            }
+          }
+        }
+        mask[y][Math.floor(x / 32)] = bits;
+      }
+    }
+
+    return new CollisionMask(w, h, mask);
+  }
+
+  static fromColor(data: ImageData) {
+    const w = data.width;
+    const h = data.height;
+    const mask: Uint32Array[] = [];
+
+    for (var y = 0; y < h; ++y) {
+      mask[y] = new Uint32Array(Math.ceil(w / 32));
+      for (var x = 0; x < w; x += 32) {
+        var bits = 0;
+        for (var bit = 0; bit < 32; ++bit) {
+          bits = bits << 1;
+          if (x + bit < w) {
+            if (data.data[(y * data.width + x + bit) * 4] === 0) {
+              bits += 1;
+            }
+          }
+        }
+        mask[y][Math.floor(x / 32)] = bits;
+      }
+    }
+
+    return new CollisionMask(w, h, mask);
+  }
+
+  static forRect(width: number, height: number) {
+    const w = width;
+    const h = height;
+    const mask: Uint32Array[] = [];
+
+    for (var y = 0; y < h; ++y) {
+      mask[y] = new Uint32Array(Math.ceil(w / 32));
+      for (var x = 0; x < w; x += 32) {
+        if (x + 32 < w) {
+          mask[y][Math.floor(x / 32)] = 2 ** 32 - 1;
+        } else {
+          mask[y][Math.floor(x / 32)] = (2 ** 32 - 1) << Math.floor(32 - w + x);
+        }
+      }
+    }
+
+    return new CollisionMask(w, h, mask);
+  }
+
+  /** Test if this CollisionMask-objects collides with the given other collision mask object. dx and dy specify the
+  screen coordinates of the other object, relative to this one. Note that this function performs rectangle intersection
+  check before going into the more expensive pixel-based collision detection, so there is no need to do this, yourself. */
+  collidesWith(other: CollisionMask, dx: number, dy: number): boolean {
+    // make sure, this object is the left one of the two
+    if (dx < 0) {
+      return other.collidesWith(this, -dx, -dy);
+    }
+
+    // determine collision rectangle (if any) in terms of coordinates of this
+    if (dx > this.w) return false;
+    let y1, y2;
+    if (dy < 0) {
+      // other is above
+      if (other.h < -dy) return false;
+      y1 = 0;
+      y2 = Math.min(other.h + dy, this.h);
+    } else {
+      // other is below
+      if (this.h < dy) return false;
+      y1 = dy;
+      y2 = Math.min(other.h + dy, this.h);
+    }
+
+    const lshift = dx % 32;
+    const rshift = 32 - lshift;
+    const x1scaled = Math.floor(dx / 32);
+    const x2scaled = Math.ceil(Math.min(this.w, other.w + dx) / 32);
+
+    for (let y = y1; y < y2; ++y) {
+      const trow = this.mask[y];
+      const orow = other.mask[y - dy];
+      for (let x = x1scaled; x < x2scaled; ++x) {
+        let bits = trow[x] << lshift;
+        if (rshift < 32) {
+          // note: for whatever reason, at rshift==32, rshift will turn into a no-op,
+          // silently, while actually we really want all zeros in the right fragement, then
+          bits |= trow[x + 1] >>> rshift; // Note: zero-fill rshift
+        }
+
+        // since other is known to be to the right of this, its mask is always left-aligned.
+        if (orow[x - x1scaled] & bits) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+}
