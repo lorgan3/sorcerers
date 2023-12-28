8077e2291e70621d901fb0a35c7b2c8ce5328dd2
Lennert Claeys <claeyslennert@gmail.com>
2023-12-28 13:28:08 +0100

    Add medium circle collision mask

--- src/data/collision/precomputed/circles.ts
+++ src/data/collision/precomputed/circles.ts
@@ -20,5 +20,8 @@ export const circle9x9 = CollisionMask.fromAlpha(circle9x9Canvas);
 export const ellipse9x16Canvas = createEllipseCanvas(9, 16);
 export const ellipse9x16 = CollisionMask.fromAlpha(ellipse9x16Canvas);
 
+export const circle16x16Canvas = createEllipseCanvas(16);
+export const circle16x16 = CollisionMask.fromAlpha(circle16x16Canvas);
+
 export const circle32x32Canvas = createEllipseCanvas(32);
 export const circle32x32 = CollisionMask.fromAlpha(circle32x32Canvas);
