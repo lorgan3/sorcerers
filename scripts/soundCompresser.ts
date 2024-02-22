080f413a6edb5c36daba71ea29bf3185ab451bd0
Lennert Claeys <claeyslennert@gmail.com>
2024-02-22 19:14:12 +0100

    Add script for compressing/converting sounds

---
+++ scripts/soundCompresser.ts
@@ -0,0 +1,21 @@
+import fs from "fs";
+import ffmpeg from "fluent-ffmpeg";
+
+const DIRECTORY = "./public/sound/original/";
+const OUT_DIRECTORY = "./public/sound/";
+
+const contents = fs.readdirSync(DIRECTORY);
+const files = contents.filter(
+  (file) =>
+    file.endsWith(".mp3") || file.endsWith(".wav") || file.endsWith(".flac")
+);
+
+for (let file of files) {
+  const fileName = file.split(".").slice(0, -1).join(".");
+
+  const command = ffmpeg(`${DIRECTORY}${file}`);
+  command.addInput;
+  command.addOptions(["-vn", "-ar 44100", "-ac 2", "-q:a 6"]);
+  command.addOutput(`${OUT_DIRECTORY}/${fileName}.mp3`);
+  command.run();
+}
