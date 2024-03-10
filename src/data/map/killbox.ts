f7723f00333df8d154b635790ddda2983ef2dd1d
Lennert Claeys <claeyslennert@gmail.com>
2024-03-10 16:58:08 +0100

    Remove unused imports

--- src/data/map/killbox.ts
+++ src/data/map/killbox.ts
@@ -2,10 +2,6 @@ import { Container, Graphics, Texture, TilingSprite } from "pixi.js";
 
 import { AssetsContainer } from "../../util/assets/assetsContainer";
 import { CollisionMask } from "../collision/collisionMask";
-import { Level } from "./level";
-import { Character } from "../entity/character";
-import { GenericDamage } from "../damage/genericDamage";
-import { TargetList } from "../damage/targetList";
 
 const ANIMATION_SPEED = 0.1;
 const RISE_SPEED = 1;
