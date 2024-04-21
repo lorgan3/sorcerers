56daf65338131989404370b5239f9dc341022a9c
Lennert Claeys <claeyslennert@gmail.com>
2024-04-21 18:49:03 +0200

    Clean up properly on disconnect

--- src/data/network/index.ts
+++ src/data/network/index.ts
@@ -1,4 +1,4 @@
-import { Ticker } from "pixi.js";
+import { Ticker, UPDATE_PRIORITY } from "pixi.js";
 
 import { Server } from "./server";
 import { Level } from "../map/level";
@@ -8,16 +8,27 @@ import { Map } from "../map";
 
 export const PEER_ID_PREFIX = "sorcerers-";
 
-export const connect = async (target: HTMLElement, map: Map) => {
+export const connect = async (
+  target: HTMLElement,
+  map: Map,
+  onBack: () => void
+) => {
   const level = new Level(target, map);
   const controller = new KeyboardController(level.viewport);
 
-  Manager.instance.connect(controller);
+  const ticker = (dt: number) => {
+    Manager.instance.tick(dt, Ticker.shared.deltaMS);
+  };
+
+  const handleBack = () => {
+    Ticker.shared.remove(ticker, null);
+    onBack();
+  };
+
+  Manager.instance.connect(controller, handleBack);
   await Server.instance?.start();
 
-  Ticker.shared.add((dt) => {
-    Manager.instance.tick(dt, Ticker.shared.deltaMS);
-  });
+  Ticker.shared.add(ticker, null, UPDATE_PRIORITY.LOW);
 
   return controller;
 };
