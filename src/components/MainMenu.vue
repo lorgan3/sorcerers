a10ec478af50ad9dd22e2d6068860ff35392eef5
Lennert Claeys <claeyslennert@gmail.com>
2024-06-01 13:23:34 +0200

    Add link to github

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -31,6 +31,31 @@ import { RouterLink } from "vue-router";
           <button class="primary" @click="navigate">Credits</button>
         </RouterLink>
       </li>
+      <li>
+        <a
+          href="https://github.com/lorgan3/sorcerers"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button class="primary">
+            Issues/Feedback
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              class="github-icon"
+            >
+              <path
+                fill="currentColor"
+                d="M6 2h12v2H6V2Zm4 12H8v-2h2v2Zm4 0v2h-4v-2h4Zm0 0v-2h2v2h-2Z"
+              />
+              <path
+                fill="currentColor"
+                d="M6 6V4H4v2H2v12h2v2h2v2h12v-2h2v-2h2V6h-2V4h-2v2h-2v2H8V6H6Zm2 6v-2h8v2h2V6h2v12h-2v2h-2v-4h-2v4h-4v-4H6v2h2v2H6v-2H4v-2h2v-2H4V6h2v6h2Z"
+              />
+            </svg>
+          </button>
+        </a>
+      </li>
     </ul>
   </div>
   <RouterLink to="/spellbook" v-slot="{ navigate }">
@@ -54,6 +79,7 @@ import { RouterLink } from "vue-router";
       font-size: 32px;
       padding: 10px;
       letter-spacing: 1.5px;
+      position: relative;
     }
   }
 }
@@ -65,5 +91,12 @@ import { RouterLink } from "vue-router";
   img {
     scale: 2;
   }
+}
+
+.github-icon {
+  width: 22px;
+  position: absolute;
+  right: 6px;
+  top: 12px;
 }
 </style>
